6666e4b6ee714493eb53da6b7571b4d5e77ec772
playdigit <playdigit@gmail.com>
2023-11-03 20:32:57 +0800

    格式化

--- fast_server.py
+++ fast_server.py
@@ -1,3 +1,10 @@
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi import Body, FastAPI, Query, Response, Request
 import asyncio
 from enum import Enum
 import os
@@ -20,6 +27,7 @@ import logging
 
 
 logging.basicConfig(level=logging.INFO)
+
 
 def log_device_usage(msg, use_cuda=True):
     import psutil
@@ -90,7 +98,7 @@ def infer(text, sdp_ratio, noise_scale, noise_scale_w, length_scale, sid, langua
             .numpy()
         )
         del x_tst, tones, lang_ids, bert, ja_bert, x_tst_lengths, speakers
-        torch.cuda.empty_cache()        
+        torch.cuda.empty_cache()
         return audio
 
 
@@ -128,7 +136,7 @@ if sys.platform == "darwin" and torch.backends.mps.is_available():
     os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 else:
     dev = "cuda"
-    
+
 net_g = SynthesizerTrn(
     len(symbols),
     hps.data.filter_length // 2 + 1,
@@ -138,10 +146,11 @@ net_g = SynthesizerTrn(
 ).to(dev)
 _ = net_g.eval()
 
-_ = utils.load_checkpoint("logs/genshin_mix/G_19000.pth", net_g, None, skip_optimizer=True)
-from fastapi import Body, FastAPI, Query, Response, Request
+_ = utils.load_checkpoint("logs/genshin_mix/G_19000.pth",
+                          net_g, None, skip_optimizer=True)
 
-async def tts_fn(request:Request, text, speaker, sdp_ratio, noise_scale, noise_scale_w, length_scale, language):
+
+async def tts_fn(request: Request, text, speaker, sdp_ratio, noise_scale, noise_scale_w, length_scale, language):
     slices = text.split("|")
     audio_list = []
     log_device_usage("=============== before text infer")
@@ -151,15 +160,16 @@ async def tts_fn(request:Request, text, speaker, sdp_ratio, noise_scale, noise_s
         for slice in slices:
             if await request.is_disconnected():
                 raise Exception("request disconnect")
-            
-            audio = infer(slice, sdp_ratio=sdp_ratio, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=length_scale, sid=speaker, language=language)
+
+            audio = infer(slice, sdp_ratio=sdp_ratio, noise_scale=noise_scale,
+                          noise_scale_w=noise_scale_w, length_scale=length_scale, sid=speaker, language=language)
             audio_list.append(audio)
             silence = np.zeros(hps.data.sampling_rate)  # 生成1秒的静音
             audio_list.append(silence)  # 将静音添加到列表中
-            
-            index+=1
+
+            index += 1
             logging.info(f"slice finished: {index}")
-            
+
     audio_concat = np.concatenate(audio_list)
     log_device_usage("=============== after text infer")
     return (hps.data.sampling_rate, audio_concat)
@@ -167,32 +177,29 @@ async def tts_fn(request:Request, text, speaker, sdp_ratio, noise_scale, noise_s
 
 class Language(str, Enum):
     zh = "ZH"
-    jp = "JP"    
+    jp = "JP"
 
-class AudioFormat(str,Enum):
+
+class AudioFormat(str, Enum):
     wav = "wav"
     mp3 = "mp3"
-    
+
+
 loop = asyncio.get_event_loop()
 
 # 使用 json 传参数时
+
+
 class TTSParams(BaseModel):
-    text:str
+    text: str
     # fmt:AudioFormat=AudioFormat.wav
     # speaker:str="bcsz"
     # sdp_ratio:float=0.2
     # noise_scale:float=0.5
     # noise_scale_w:float=0.6
     # length_scale:float=1.2
-    # language:Language=Language.zh    
-    
+    # language:Language=Language.zh
 
-from fastapi.openapi.docs import (
-    get_redoc_html,
-    get_swagger_ui_html,
-    get_swagger_ui_oauth2_redirect_html,
-)
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -209,32 +216,34 @@ async def custom_swagger_ui_html():
         swagger_css_url="/static/swagger-ui.css",
     )
 
+
 def async_tts_fn(*args):
     loop = asyncio.new_event_loop()
     results = loop.run_until_complete(tts_fn(*args))
     loop.close()
     return results
 
+
 @app.post("/tts")
-async def request_tts(request:Request, body:TTSParams=Body(...), fmt:AudioFormat=Query(AudioFormat.wav), speaker:str=Query("bcsz"), sdp_ratio:float=Query(0.2), noise_scale:float=Query(0.5),
-                      noise_scale_w:float=Query(0.6), length_scale:float=Query(1.2), language:Language=Query(Language.zh)):
+async def request_tts(request: Request, body: TTSParams = Body(...), fmt: AudioFormat = Query(AudioFormat.wav), speaker: str = Query("年轻女士"), sdp_ratio: float = Query(0.2), noise_scale: float = Query(0.5),
+                      noise_scale_w: float = Query(0.6), length_scale: float = Query(1.2), language: Language = Query(Language.zh)):
     text = body.text.replace("/n", "")
     rate, audio = await loop.run_in_executor(None, async_tts_fn,
-        request,
-        text, 
-        speaker,
-        sdp_ratio,
-        noise_scale,
-        noise_scale_w,
-        length_scale,
-        language+""
-    )
+                                             request,
+                                             text,
+                                             speaker,
+                                             sdp_ratio,
+                                             noise_scale,
+                                             noise_scale_w,
+                                             length_scale,
+                                             language+""
+                                             )
 
     with BytesIO() as wav:
         wavfile.write(wav, rate, audio)
         torch.cuda.empty_cache()
         if fmt == "wav":
-            response =  Response(wav.getvalue(), media_type="audio/wav")
+            response = Response(wav.getvalue(), media_type="audio/wav")
             response.headers["Content-Disposition"] = 'attachment; filename="audio.wav"'
             return response
         wav.seek(0, 0)
@@ -245,11 +254,11 @@ async def request_tts(request:Request, body:TTSParams=Body(...), fmt:AudioFormat
             )
             response.headers["Content-Disposition"] = 'attachment; filename="audio.mp3"'
             return response
-               
+
 
 if __name__ == '__main__':
-    
+
     import uvicorn
     # 等于通过 uvicorn 命令行 uvicorn 脚本名:app对象 启动服务：
     #  uvicorn xxx:app --reload
-    uvicorn.run('fast_server:app',host="0.0.0.0", port=8301, reload=True)
+    uvicorn.run('fast_server:app', host="0.0.0.0", port=8310, reload=True)
